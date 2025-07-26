3a65e186d44b75a51d5d50dff22c3d6bd101d2d5
Jarie 水栓 (Suisen) <jariesuicune@msn.com>
2025-07-26 09:21:43 -0600

    as it is now

--- week04/scripts/arrayfunctions.js
+++ week04/scripts/arrayfunctions.js
@@ -1,5 +1,10 @@
+
+
+// USE THIS LIST FOR EACH OF THE FOLLOWING ARRAY FUNCTIONS
 let names = ['Nancy', 'Blessing', 'Jorge', 'Svetlana', 'Bob'];
 
+
+// FILTER
 // Using the array.filter() method, create an array named namesB with only those names from the name array that start with the character 'B'.
 
 function filter(list) {
@@ -10,7 +15,7 @@ function filter(list) {
 
 }
 
-
+// MAP
 // Using the array.map() method, create a new array named namesLength that contains the length of each name in the names array.Expected output is[5, 8, 5, 8, 3].
 
 function map(list) {
@@ -21,12 +26,13 @@ function map(list) {
 
 }
 
+// REDUCE
 // Using the names.reduce() method, create an expression that returns the average string length of the names in the names array.Expected output is 5.8.
 
 function reduce(list) {
 
     const initialvalue = 0;
-    let x = 0;
+    // let x = 0;
     const reducedtotal = list.reduce(
         // WHAT???
 
@@ -38,13 +44,19 @@ function reduce(list) {
     return average
 }
 
-
+// OBTAIN DESTINATION FROM HTML
 const printfilter = document.querySelector("#filter");
 const printmap = document.querySelector("#map");
 const printreduce = document.querySelector("#reduce");
 
+// OUTPUT TO HTML DESTINATION
+printfilter.innerHTML = `<span class="highlight">\ ${filter(names)} \</span>`;
+printmap.innerHTML = `<span class="highlight">\ ${map(names)} \</span>`;
+printreduce.innerHTML = `<span class="highlight">\ ${reduce(names)} \</span>`;
 
-printfilter.innerHTML = `<span class="highlight">${filter(names)}</span>`;
-printmap.innerHTML = `<span class="highlight">${map(names)}</span>`;
-printreduce.innerHTML = `<span class="highlight">${reduce(names)}</span>`;
+
+
+
+// USE THIS METHOD TO COMBINE TEXT WITH VARIABLES
+`Welcome to the ${courseName} ${stuFirstName}!`;
 
